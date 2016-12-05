c03753db6a017343f032c59baf51076846eaea57
David Oddoye <oddoyedavid@gmail.com>
2016-12-05 21:42:43 +0000

    Updated server initialisation and refernces

--- app.js
+++ app.js
@@ -1,46 +1,67 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var Hapi = require('hapi');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+var app = new Hapi.Server();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.register(require('vision'), function (err) {
+  if (err) {
+    throw err;
+  }
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  app.views({
+    engines: {
+      jade: require('jade')
+    },
+    path: __dirname + '/views',
+    compileOptions: {
+      pretty: true
+    }
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const loggerOptions = {
+  ops: {
+    interval: 5000
+  },
+  reporters: {
+    myConsoleReporter: [{
+      module: 'good-squeeze',
+      name: 'Squeeze',
+      args: [{
+        log: '*',
+        response: '*'
+      }]
+    }, {
+      module: 'good-console'
+    }, 'stdout']
+  }
+};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Register Logger
+app.register({
+  register: require('good'),
+  loggerOptions,
+}, function (err) {
+  if (err) {
+    throw err;
+  }
+});
+
+// Register static file serving
+app.register(require('inert'), function (err) {
+  if (err) {
+    throw err;
+  }
+
+  app.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: 'public'
+      }
+    }
+  });
 });
 
 module.exports = app;
